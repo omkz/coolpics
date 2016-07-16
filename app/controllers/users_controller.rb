706fead80aebca1780c1e6032e5883086b5253f3
omz <omenkzz@gmail.com>
2016-07-16 09:59:31 +0700

    add title in user follow page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,12 +2,14 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
 
   def following
+    @title = "Following:"
     @user  = User.find(params[:id])
     @users = @user.following.paginate(page: params[:page])
     render 'show_follow'
   end
 
   def followers
+    @title = "Followers:"
     @user  = User.find(params[:id])
     @users = @user.followers.paginate(page: params[:page])
     render 'show_follow'
