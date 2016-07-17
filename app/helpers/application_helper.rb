02398e08087a93112795f2d5ab14d78142339384
omz <omenkzz@gmail.com>
2016-07-17 12:28:13 +0700

    make username & avatar clickable on homepage

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,8 @@ module ApplicationHelper
   def resource_class
     User
   end
+
+  def link_to_image(image_path, target_link,options={})
+    link_to(image_tag(image_path, :border => "0"), target_link, options)
+  end
 end
