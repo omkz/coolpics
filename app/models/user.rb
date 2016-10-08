51414a918ecb5edc17496ff4d91b0e150fd29e50
kurnia muhamad <omenkzz@gmail.com>
2016-10-09 05:25:35 +0700

    make and update carrier uploader for avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ActiveRecord::Base
   validates :username, presence: true, uniqueness: true, length: {minimum: 3, maximum: 17}
 
   acts_as_voter
-  mount_uploader :avatar, PictureUploader
+  mount_uploader :avatar, AvatarUploader
 
   has_many :posts
 
