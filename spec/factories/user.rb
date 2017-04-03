21706d16eb413a59a2f07a24c5bd630e35ef4747
kurnia muhamad <omenkzz@gmail.com>
2017-04-03 07:25:14 +0700

    setup devise's test helpers for controller tests

---
+++ spec/factories/user.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    email { Faker::Internet.email }
+    username { Faker::Internet.user_name }
+    password "password"
+    password_confirmation "password"
+  end
+end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -31,7 +31,7 @@ end
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
 #
-# Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
 # Checks for pending migration and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
@@ -40,6 +40,10 @@ ActiveRecord::Migration.maintain_test_schema!
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
+
+  config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include Devise::Test::ControllerHelpers, type: :view
+  config.extend ControllerMacros, :type => :controller
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
