95a10f2a7cb6a99717503e4e964bc7056c056339
kurnia muhamad <omenkzz@gmail.com>
2017-04-03 07:31:24 +0700

    setup devise's test helpers for integration tests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -41,9 +41,13 @@ RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
+  #devise's test helpers for controller tests
   config.include Devise::Test::ControllerHelpers, type: :controller
   config.include Devise::Test::ControllerHelpers, type: :view
   config.extend ControllerMacros, :type => :controller
+
+  #devise's test helpers for integration tests
+  config.include Devise::Test::IntegrationHelpers, type: :feature
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
