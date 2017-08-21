615ab3f397864bf23fbb029b9df3fd1bc9288ad3
kurnia muhamad <omenkzz@gmail.com>
2017-08-21 10:41:02 +0700

    auth user for create a comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,6 @@
 class CommentsController < ApplicationController
+  before_action :authenticate_user!, only: [:create]
+
   def create
     @post = Post.friendly.find(params[:post_id])
     @comment = @post.comments.create(comment_params)
