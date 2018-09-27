dfe28e5e0faf9ab0c6a3e36c9a401fb6a53f2797
Kurnia Muhamad <omenkzz@gmail.com>
2018-09-27 20:43:13 +0700

    fix comment system issue

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,12 +3,14 @@ class CommentsController < ApplicationController
 
   def create
     @post = Post.friendly.find(params[:post_id])
-    @comment = @post.comments.create(comment_params)
+    @post.comments.create(comment_params) do |post|
+      post.user = current_user
+    end
     redirect_to post_path(@post)
   end
  
   private
     def comment_params
-      params.require(:comment).permit(:body, :user_id)
+      params.require(:comment).permit(:body)
     end
 end
