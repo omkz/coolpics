17b9920f500ec5f6cd1c7951a32ebe9014d2db2f
Kurnia Muhamad <omenkzz@gmail.com>
2018-10-02 18:39:29 +0700

    descending post by date

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,7 +5,7 @@ class HomeController < ApplicationController
     if params[:search]
       @posts = Post.search(params[:search]).order("created_at DESC").page(params[:page]).per(10)
     else
-      @posts = Post.includes(:user).page(params[:page]).per(10)
+      @posts = Post.order("created_at DESC").includes(:user).page(params[:page]).per(10)
     end
   end
 
