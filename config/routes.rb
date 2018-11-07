6d432a987b50f62318db8e4c3504c19fafd2f2f0
Kurnia Muhamad <omenkzz@gmail.com>
2018-11-07 22:54:14 +0700

    update notification url

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  get 'notifications/index'
+  get "/notifications",  to: 'notifications#index'
 
   get "/search", to: 'home#index'
 
