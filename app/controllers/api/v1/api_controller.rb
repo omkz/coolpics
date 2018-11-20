760e6cdd6e7f7a29a89616a33bd1071d5bfb511b
Kurnia Muhamad <omenkzz@gmail.com>
2018-11-21 01:03:13 +0700

    styling AllPost component

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -2,10 +2,10 @@ module Api::V1
   class ApiController < ActionController::API
     respond_to :json
 
-    private
+    # private
 
-    def current_user
-      @current_user ||= User.find(doorkeeper_token[:resource_owner_id])
-    end
+    # def current_user
+    #   @current_user ||= User.find(doorkeeper_token[:resource_owner_id])
+    # end
   end
 end
--- app/javascript/posts/components/AllPosts.jsx
+++ app/javascript/posts/components/AllPosts.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from "react";
+import ReactDOM from "react-dom";
 
 class AllPosts extends React.Component {
   constructor(props) {
@@ -8,28 +8,29 @@ class AllPosts extends React.Component {
       posts: []
     };
   }
-  componentDidMount(){
-    fetch('/api/v1/posts')
-      .then((response) => {return response.json()   })
-      .then((data) => { this.setState({ posts: data })});
+  componentDidMount() {
+    fetch("/api/v1/posts")
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        this.setState({ posts: data });
+      });
   }
-  
-  render(){
-    var posts = this.state.posts.map((post) => {
-     return(
-      <div key={post.id}>
-       <h1>{post.title}</h1>
-       <p><img src={post.image.standard.url}/></p>
-       
-      </div>
-     )
-    })
-    return(
-     <div>
-      {posts}
-     </div>
-    )
-   }
+
+  render() {
+    var posts = this.state.posts.map(post => {
+      return (
+        <div className={"col-lg-12 well"} key={post.id}>
+          <h1>{post.title}</h1>
+          <p>
+            <img src={post.image.standard.url} />
+          </p>
+        </div>
+      );
+    });
+    return <div className="row">{posts}</div>;
+  }
 }
 
-export default AllPosts
+export default AllPosts;
