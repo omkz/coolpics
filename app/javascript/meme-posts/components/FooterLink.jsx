871f497086bb7bd99151feea8f5c20ca714225bf
Kurnia Muhamad <omenkzz@gmail.com>
2018-11-24 17:13:08 +0700

    update FooterLink Component style

--- app/javascript/meme-posts/components/FooterLink.jsx
+++ app/javascript/meme-posts/components/FooterLink.jsx
@@ -9,9 +9,12 @@ class FooterLink extends React.Component {
   render() {
     return (
       <div>
-         <p><a href={ this.props.post.url} data-turbolinks="false">comments</a></p>
-         <p><a href={ "https://www.facebook.com/sharer/sharer.php?u=" + this.props.post.url } data-turbolinks="false">facebook</a></p>
-         <p><a href={ "http://twitter.com/home?status=" + this.props.post.url } data-turbolinks="false">twitter</a></p>
+         <a href={ this.props.post.url} data-turbolinks="false" className="btn btn-info">comments</a>
+         <a href={ "https://www.facebook.com/sharer/sharer.php?u=" + this.props.post.url } data-turbolinks="false" className="btn btn--fb">
+           <i class="fa fa-facebook fa-fw"></i>Facebook
+         </a>
+         <a href={ "http://twitter.com/home?status=" + this.props.post.url } data-turbolinks="false" className="btn btn--twitter">
+         <i class="fa fa-twitter fa-fw"></i>Twitter </a>
       </div>
     );
   }
