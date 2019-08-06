d4611725cc2f7f6cb51e5247890cfe408c96f41f
Kurnia Muhamad <omenkzz@gmail.com>
2019-08-06 21:29:03 +0700

    Update CMS for Post

--- app/admin/post.rb
+++ app/admin/post.rb
@@ -1,8 +1,14 @@
 ActiveAdmin.register Post do
+ 
+#  scope :all, default: true
+ filter :title
+ 
+
  index do
     column :title
     column :image
     column "Created Date", :created_at
+    actions
     
   end
 end
